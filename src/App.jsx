c580526f9e77d1c1eb033838608eb5dacf073534
Umidjon Jumaqulov <u.jumaqulov@newuu.uz>
2026-01-19 05:35:46 +0500

    Change BrowserRouter to HashRouter in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 // --- Layouts ---
 import Layout from './components/Layout';
